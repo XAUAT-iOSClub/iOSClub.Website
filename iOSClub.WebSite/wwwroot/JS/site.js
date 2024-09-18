e3a5788179cef32d4326f935257450f6ffaab25a
李嘉俊 <2383690354@qq.com>
2024-09-18 17:38:24 +0800

    注册跳转QQ无效

--- iOSClub.WebSite/wwwroot/JS/site.js
+++ iOSClub.WebSite/wwwroot/JS/site.js
@@ -3,17 +3,12 @@
     return !!/MicroMessenger/i.test(ua)
 }
 
-function isQQ() {
-    let ua = navigator.userAgent.toLowerCase();
-    return ua.indexOf('qq/') !== -1;
-}
-
 function NavigateTo(url, webUrl) {
     const u = navigator.userAgent;
     const isAndroid = u.indexOf('Android') > -1 || u.indexOf('Adr') > -1; //android终端
     const isiOS = !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/); //ios终端
     let result = isAndroid || isiOS;
-    if(isQQ() || !result){
+    if(u.indexOf('MQQBrowser') !== -1 || !result){
         window.open(webUrl)
     }else{
         window.open(url)
